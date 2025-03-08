6621076f4631c30ed22f0c080a5034af2d3ae1b9
Taowen wang <41105739+William-wAng618@users.noreply.github.com>
2025-03-08 00:58:13 -0500

    Update run_simulation.sh

--- scripts/run_simulation.sh
+++ scripts/run_simulation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python 0pythonRunpythonTools/eval_queue.py \
-    --exp_path xxx  \
-    --max_concurrent_tasks  2 \
-    --trials 10
+python evaluation_tool/eval_queue_single_four_spec.py \
+    --exp_path PATH TO/fe28658a-4a27-4ffa-82c4-94d44ffc9d48 \
+    --cudaid 0 \
+    --trials 50 \
+    --max_concurrent_tasks 1 \
+    --task libero_10 \     
